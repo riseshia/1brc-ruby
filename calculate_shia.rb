5e5032495ece7ab12fd1797035799a0a662bb0c4
Shia <rise.shia@gmail.com>
2024-03-17 09:01:01 +0900

    streaming..?

--- calculate_shia.rb
+++ calculate_shia.rb
@@ -3,23 +3,36 @@
 TARGET_PATH = ARGV.first || "measurements.txt"
 BUFFER_SIZE = 110
 
+Station = Struct.new(:tmin, :tmax, :tsum, :tnum)
+
 def main
-  temp_per_station = Hash.new { |h, k| h[k] = [] }
+  temp_per_station = Hash.new { |h, k| h[k] = Station.new(100.0, -101.0, 0.0, 0) }
 
   File.open(TARGET_PATH) do |f|
     f.each_line(BUFFER_SIZE) do |row|
-      name, temp = row.split(";")
-      temp_per_station[name] << temp.to_f
+      name, temp_s = row.split(";")
+      temp = temp_s.to_f
+      s = temp_per_station[name]
+      s.tmin = temp if temp < s.tmin
+      s.tmax = temp if temp > s.tmax
+      s.tsum += temp
+      s.tnum += 1
     end
 
+    putc "{"
+    started = false
     calculated = temp_per_station.keys.sort.map do |name|
-      min = temp_per_station[name].min
-      max = temp_per_station[name].max
-      mean = temp_per_station[name].sum.fdiv(temp_per_station[name].size).round(1)
-      format("%s=%.1f/%.1f/%.1f", name, min, mean, max)
-    end
+      if started
+        print ", "
+      else
+        started = true
+      end
 
-    puts "{#{calculated.join(', ')}}"
+      s = temp_per_station[name]
+      mean = s.tsum.fdiv(s.tnum).round(2).round(1)
+      print format("%s=%.1f/%.1f/%.1f", name, s.tmin, mean, s.tmax)
+    end
+    puts "}"
   end
 end
 
